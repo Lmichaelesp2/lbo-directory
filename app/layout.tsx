1b790d5ea75edc19669a0e814042656552e17e29
Louis Espinoza <themobilecoach@gmail.com>
2026-06-19 22:45:42 -0500

    Add Sitebehavior analytics tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,30 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Sitebehavior analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  if (window.location && window.location.search && window.location.search.indexOf('capture-sitebehaviour-heatmap') !== -1) {
+                    sessionStorage.setItem('capture-sitebehaviour-heatmap', '_');
+                  }
+
+                  var sbSiteSecret = '576234fd-9610-43a2-bdb5-b36bd9f53b50';
+                  window.sitebehaviourTrackingSecret = sbSiteSecret;
+                  var scriptElement = document.createElement('script');
+                  scriptElement.defer = true;
+                  scriptElement.id = 'site-behaviour-script-v2';
+                  scriptElement.src = 'https://sitebehaviour-cdn.fra1.cdn.digitaloceanspaces.com/index.min.js?sitebehaviour-secret=' + sbSiteSecret;
+                  document.head.appendChild(scriptElement);
+                } catch (e) { console.error(e); }
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-screen flex flex-col">{children}</body>
     </html>
   );
